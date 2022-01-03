92e83489385316955971457f72f07505ed291be8
Roatt Tilma <64038600+roatt-tilma@users.noreply.github.com>
2022-01-03 19:49:44 +0545

    Time: 0 ms (100.00%), Space: 6.1 MB (99.68%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        vector<char> stack;
+        int len = s.size();
+        char end_brac = ' ';
+        for (int i = 0; i < len; i++){
+            if (s[i] == '(' || s[i] == '{' || s[i] == '['){
+                stack.push_back(s[i]);
+            }
+            else{
+                end_brac = s[i];
+                if (stack.size() == 0) return false;
+                if (end_brac == ')' && stack.back() != '(' 
+                    || end_brac == '}' && stack.back() != '{'
+                    || end_brac == ']' && stack.back() != '['){
+                    return false;
+                }else{
+                    stack.pop_back();
+                }
+            }
+        }
+        if (stack.size() != 0) return false;
+        return true;
+    }
+};
