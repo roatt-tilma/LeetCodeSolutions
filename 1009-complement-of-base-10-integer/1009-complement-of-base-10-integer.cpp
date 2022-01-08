2293831db6f3b5d246b274608a3d692362d25bd8
Roatt Tilma <64038600+roatt-tilma@users.noreply.github.com>
2022-01-08 20:08:45 +0545

    Time: 0 ms (100.00%), Space: 6 MB (41.57%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -2,9 +2,6 @@ class Solution {
 public:
     int bitwiseComplement(int n) {
         if (n == 0) return 1;
-        int power = floor(log2(n)) + 1;
-        int toXORWith = pow(2, power) - 1;
-        int answer = n^toXORWith;
-        return answer;
+        return (n^(int)(pow(2, (floor(log2(n)) + 1)) - 1));
     }
 };
