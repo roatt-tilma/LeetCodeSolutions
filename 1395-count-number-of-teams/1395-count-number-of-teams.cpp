b00f699ff5fa486fd8fa04f24513e9ec01ba8da5
Roatt Tilma <64038600+roatt-tilma@users.noreply.github.com>
2022-01-14 22:39:49 +0545

    Time: 188 ms (25.08%), Space: 9 MB (54.16%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.cpp
+++ 1395-count-number-of-teams/1395-count-number-of-teams.cpp
@@ -3,15 +3,6 @@ public:
     int getCount(vector<int>& rating, int n){
         int count = 0;
         int j,k;
-        
-        //for (int i = 0; i < n-2; i++){
-        //    for (int j = i+1; j < n-1; j++){
-        //        for (int k = j+1; k < n; k++){
-        //            if (rating[i] < rating[j] && rating[j] < rating[k]) count++;
-        //            else if (rating[i] > rating[j] && rating[j] > rating[k]) count++;
-        //        }
-        //   }
-        //}
         int leftSmall;
         int rightSmall;
         int leftLarge;
