c4608df64256ae963352093d4687aa5ced7770c8
Roatt Tilma <64038600+roatt-tilma@users.noreply.github.com>
2022-01-15 19:37:01 +0545

    Time: 946 ms (5.05%), Space: 26.3 MB (98.62%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -24,5 +24,35 @@ public:
         }
         if (lflag) return 0;
         return r - l + 1;
+        
+        //int l = 0, r = nums.size() - 1;
+        
+        //for (int i = 0; i < r; i++){
+          //  if (nums[i] <= nums[i+1]) continue;
+         //   l = i+1;
+        //    break;
+       // }
+        
+        //for (int i = l-1; i > 0; i--){
+            //if (nums[i] >= nums[l]) continue;
+           // l = i+1;
+         //   break;
+       // }
+        
+       // if (l==0) return 0;
+        
+        //for (int i = r; i > 0; i--){
+            //if(nums[i] >= nums[i-1]) continue;
+           // r = i-1;
+         //   break;
+       // }
+        
+       // for (int i = r + 1; i < r; i++){
+        //    if (nums[i] < nums[r]) continue;
+        //    r = i - 1;
+        //    break;
+        //}
+        
+        //return r - l + 3;
     }
 };
