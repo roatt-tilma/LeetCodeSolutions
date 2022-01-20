2733d6b69e080c63742ba511529f320abf0448dd
Roatt Tilma <64038600+roatt-tilma@users.noreply.github.com>
2022-01-20 19:40:38 +0545

    Time: 136 ms (5.10%), Space: 18.9 MB (16.35%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.cpp
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -9,36 +9,38 @@ public:
         }
         return count;
     }
+    
     int minEatingSpeed(vector<int>& piles, int h) {
         sort(piles.begin(), piles.end());
         int count,l,r,m;
         int len = piles.size();
         for (int i = 0; i < len; i++){
             count = findCount(piles,piles[i],i); 
+            if (count > h) continue;
             if (count == h) return piles[i];
-            if (count < h){
-                if (i == 0){
-                  l = 0;
-                  r = piles[i];
-                } 
-                else{
-                    l = piles[i-1];
-                    r = piles[i];
-                }
-                while (r-l != 1){
-                    m = (r+l)/2; 
-                    count = findCount(piles, m, i-1);
-                    if (count == h){
-                        for (int k = m; k >= l; k--){
-                            count = findCount(piles,k,i-1);
-                            if (count > h) return k+1;
-                        }
-                    }
-                    if (count < h) r = m;
-                    else l = m;
-                }
-                return r;
+            if (i == 0){
+                l = 0;
+                r = piles[i];
+            } 
+            else{
+                l = piles[i-1];
+                r = piles[i];
             }
+            
+            while (r-l != 1){
+                m = (r+l)/2; 
+                count = findCount(piles, m, i-1);
+                if (count == h){
+                    for (int k = m; k >= l; k--){
+                        count = findCount(piles,k,i-1);
+                        if (count > h) return k+1;
+                    }
+                }
+                if (count < h) r = m;
+                    else l = m;
+            }
+            return r;
+            
             
         }
         
