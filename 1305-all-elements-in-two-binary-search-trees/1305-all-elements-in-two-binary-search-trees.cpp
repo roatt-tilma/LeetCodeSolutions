bd0db539258e268d0fd90ebaef0bb441587b1f60
Roatt Tilma <64038600+roatt-tilma@users.noreply.github.com>
2022-01-26 17:54:41 +0545

    Time: 301 ms (8.82%), Space: 86.8 MB (15.36%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -11,19 +11,38 @@
  */
 class Solution {
 public:
-    void pushToVector(vector<int>& ans, TreeNode* node){
+    void pushToVector(vector<int>& vect, TreeNode* node){
         if (node == NULL) return;
         
-        pushToVector(ans, node->left);
-        ans.push_back(node->val);
-        pushToVector(ans, node->right);
+        pushToVector(vect, node->left);
+        vect.push_back(node->val);
+        pushToVector(vect, node->right);
     }
     
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
+        vector<int> vect1;
+        vector<int> vect2;
         vector<int> ans;
-        pushToVector(ans, root1);
-        pushToVector(ans, root2);
-        sort(ans.begin(), ans.end());
+        pushToVector(vect1, root1);
+        pushToVector(vect2, root2);
+        int i = 0; int j = 0;
+        int s1 = vect1.size();
+        int s2 = vect2.size();
+        
+        while(i != s1 && j != s2){
+            if(vect1[i] <= vect2[j]){
+                ans.push_back(vect1[i]);
+                i++;
+            }
+            else {
+                ans.push_back(vect2[j]);
+                j++;
+            }
+        }
+        
+        if (i == s1 && j == s2) return ans;
+        if (i == s1) for(i = j; i < s2; i++) ans.push_back(vect2[i]);
+        else for (j = i; j < s1; j++) ans.push_back(vect1[j]);
         return ans;
     }
 };
