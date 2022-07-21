1eb5084208d70b22cf49180c0f2e15c72d46c612
Roatt Tilma <64038600+roatt-tilma@users.noreply.github.com>
2022-07-21 22:37:55 +0530

    Time: 27 ms (32.45%), Space: 13.2 MB (52.88%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,31 +1,50 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        map<int, int> rowZero;
-        map<int, int> colZero;
+        int rowNo = matrix.size();
+        int colNo = matrix[0].size();
         
-        for(int i = 0; i < matrix.size(); i++){
-            for(int j = 0; j < matrix[i].size(); j++){
+        int colFlag = 0;
+        int rowFlag = 0;
+        
+        for(int i = 0; i < rowNo; i++){
+            for(int j = 0; j < colNo; j++){
                 if(matrix[i][j] == 0){
-                    colZero[j] = 1;
-                    rowZero[i] = 1;
+                    if(i == 0) rowFlag = 1;
+                    if(j == 0) colFlag = 1;
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
                 }
             }
         }
         
-        for(int i = 0; i < matrix.size(); i++){
-            if(rowZero[i] == 1){
-                for(int j = 0; j < matrix[i].size(); j++){
+        for(int j = 1; j < colNo; j++){
+            if(matrix[0][j] == 0){
+                for(int i = 1; i < rowNo; i++){
+                    matrix[i][j] = 0;                    
+                }
+            }
+        }
+            
+        for(int i = 1; i < rowNo; i++){
+            if(matrix[i][0] == 0){
+                for(int j = 1; j < colNo; j++){
                     matrix[i][j] = 0;
                 }
-            }    
-            else{
-                for(int j = 0; j < matrix[i].size(); j++){
-                    if(colZero[j] == 1){
-                        matrix[i][j] = 0;
-                    }
-                }
             }
         }
+
+        if(rowFlag){
+            for(int j = 0; j < colNo; j++){
+                matrix[0][j] = 0;
+            }
+        }
+        
+        if(colFlag){
+            for(int i = 0; i < rowNo; i++){
+                matrix[i][0] = 0;
+            }
+        }
+        
     }
 };
