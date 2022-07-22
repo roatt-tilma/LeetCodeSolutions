6e8a12460c32a53611c8a90aefe5564bea1ba555
Roatt Tilma <64038600+roatt-tilma@users.noreply.github.com>
2022-07-22 22:17:49 +0530

    Time: 10 ms (29.27%), Space: 12.2 MB (36.60%) - LeetHub

---
+++ 31-next-permutation/31-next-permutation.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    
+    int getSwapIndex(int i, int l, vector<int> nums){
+        int comp = nums[l];
+        
+        int swapIndex = i;
+        
+        for(int j = i; j < nums.size(); j++){
+            if(nums[j] > comp && nums[j] <= nums[swapIndex]) swapIndex = j;
+        }
+        
+        
+        return swapIndex;
+    }
+    
+    void nextPermutation(vector<int>& nums) {
+        
+        if(nums.size() == 1) return;
+        
+        for(int i = nums.size() - 1; i >= 0; i--){
+            int l = i - 1;
+            
+            if(l < 0){
+                sort(nums.begin(), nums.end());
+                break;
+            };  
+            
+            if(nums[i] > nums[l]){
+                
+                int swapIndex = getSwapIndex(i, l, nums);
+                
+                int temp = nums[swapIndex];
+                nums[swapIndex] = nums[l];
+                nums[l] = temp;
+                
+                sort(nums.begin() + i, nums.end());
+                break;
+            }
+        }
+        
+        
+    }
+};
