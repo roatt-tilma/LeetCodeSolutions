d9019f5220020733bc3ca1f47c686fdc69ec4c35
Roatt Tilma <64038600+roatt-tilma@users.noreply.github.com>
2022-07-26 00:10:19 +0530

    Time: 10 ms (8.34%), Space: 9 MB (71.80%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -3,43 +3,23 @@ public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         vector<int> nums3;
         
-        int i = 0;
-        int j = 0;
+        if(n == 0) return;
         
-        if(m != 0 && n != 0){
-            while(i < m && j < n){
-                if(nums1[i] < nums2[j]){
-                    nums3.push_back(nums1[i]);
-                    i++;
-                } else{
-                    nums3.push_back(nums2[j]);
-                    j++;
+        if(m != 0){
+            for(int i = 0; i < m; i++){
+                if(nums1[i] > nums2[0]){
+                    swap(nums1[i], nums2[0]);
+                    for(int j = 1; j < n; j++){
+                        if(nums2[j] < nums2[j-1]) swap(nums2[j], nums2[j-1]);
+                        else break;
+                    }
                 }
-            }
-
-            if(i == m){
-                for(int k = j; k < n; k++){
-                    nums3.push_back(nums2[k]);
-                }
-            }
-
-            if(j == n){
-                for(int k = i; k < m; k++){
-                    nums3.push_back(nums1[k]);
-                }
-            }
-
-            for(int i = 0; i < m+n; i++){
-                nums1[i] = nums3[i];
-            }
+            }    
         }
         
-                
-        if(m == 0){
-            for(int i = 0; i < n; i++){
-                nums1[i] = nums2[i];
-            }
-        }
         
+        for(int i = 0; i < n; i++){
+            nums1[m+i] = nums2[i];
+        }
     }
 };
