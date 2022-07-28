ac4b397304971ae97cda0e7e6b84fac9b72fb2ce
Roatt Tilma <64038600+roatt-tilma@users.noreply.github.com>
2022-07-28 18:11:22 +0530

    Time: 5 ms (18.38%), Space: 5.9 MB (73.52%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -10,8 +10,20 @@ public:
         return dp[i][j];
     }
     
+    
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int> (n, -1));
-        return countUPath(m, n, 0, 0, dp);
+        //vector<vector<int>> dp(m, vector<int> (n, -1));
+        //return countUPath(m, n, 0, 0, dp);
+        
+        int minimum = min(m-1,n-1);
+        
+        double ans = 1;
+        
+        for(int i = 1; i <= minimum; i++){
+            ans = ans * (m+n-2-minimum+i)/i;
+        }
+        
+        return (int)ans;
+        
     }
 };
