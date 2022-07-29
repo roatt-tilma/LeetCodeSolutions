f7646b0549420d9960bda9e7ff57e81d9c50e929
Roatt Tilma <64038600+roatt-tilma@users.noreply.github.com>
2022-07-29 16:36:41 +0530

    Time: 47 ms (70.22%), Space: 71.3 MB (81.42%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -15,49 +15,26 @@ public:
         ListNode* pAns = ans;
         int carry = 0;
         
-        ListNode* temp1 = l1;
-        ListNode* temp2 = l2;
-        
-        while(temp1->next != NULL && temp2->next !=NULL){
-            temp1 = temp1->next;
-            temp2 = temp2->next;
-        }
-        
-        if(temp1->next == NULL){
-            while(temp2->next != NULL){
-                temp1->next = new ListNode();
-                temp1 = temp1->next;
-                temp2 = temp2->next;
-            }
-        }
-        
-        if(temp2->next == NULL){
-            while(temp1->next != NULL){
-                temp2->next = new ListNode();
-                temp2 = temp2->next;
-                temp1 = temp1->next;
-            }
-        }
-        
-        while(true){
-            int s = l1->val + l2->val + carry;
+        while(l1 != NULL || l2 != NULL || carry){
+            
+            int s = carry;
+            
+            if(l1 != NULL){
+                s += l1->val;
+                l1 = l1->next;
+            } 
+            
+            if(l2 != NULL){
+                s += l2->val;
+                l2 = l2->next;
+            } 
             
             carry = s/10;
-            
-            ans->val = s%10;
-            
-            if(l1->next == NULL || l2->next == NULL){
-                if(carry == 1) ans->next = new ListNode(carry);
-                break;
-            } else{
-                ans->next = new ListNode();
-                ans = ans->next;
-            }        
-                                    
-            l1 = l1->next;
-            l2 = l2->next;
+            ans->next = new ListNode(s%10);
+            ans = ans->next;
         }
         
-        return pAns;
+        
+        return pAns->next;
     } 
 };
