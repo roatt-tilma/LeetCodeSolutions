cc8dc698f2cb62c5144cda29f233feeb69ba928f
Roatt Tilma <64038600+roatt-tilma@users.noreply.github.com>
2022-07-31 00:44:48 +0530

    Time: 8 ms (41.64%), Space: 7.6 MB (72.81%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -2,22 +2,30 @@ class Solution {
 public:
     
     void recurse(vector<int>& nums, vector<int> subset, int ind, int N, vector<vector<int>>& ans){
+        
         if(ind == N){
-            sort(subset.begin(), subset.end());
-            if(find(ans.begin(), ans.end(), subset) == ans.end()) ans.push_back(subset);
             return;
         }
         
-        subset.push_back(nums[ind]);
-        recurse(nums, subset, ind+1, N, ans);
-        subset.pop_back();
-        recurse(nums, subset, ind+1, N, ans);
+        int last = INT_MIN;
+        
+        for(int i = ind; i < N; i++){  
+            if(last != nums[i]){
+                last = nums[i];
+                subset.push_back(nums[i]);
+                ans.push_back(subset);
+                recurse(nums, subset, i+1, N, ans);
+                subset.pop_back();
+            }
+        }
+        
     }
     
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> subset;
-        
+        sort(nums.begin(), nums.end());
+        ans.push_back(subset);
         recurse(nums, subset, 0, nums.size(), ans);
         
         return ans;
