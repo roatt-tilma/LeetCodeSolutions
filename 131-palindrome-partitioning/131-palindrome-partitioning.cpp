42b91737543cad42e2d6ed61d245a351f11b5d6e
Roatt Tilma <64038600+roatt-tilma@users.noreply.github.com>
2022-07-31 17:38:22 +0530

    Time: 388 ms (15.00%), Space: 144.9 MB (15.24%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,27 +1,35 @@
 class Solution {
 public:
-
-    bool isPalindrome(string s, int start, int end){
+    
+    bool isPalindrome(string s, int start, int end, vector<vector<int>>& dp){
         
+        if(dp[start][end] == -1) return false;
+        if(dp[start][end] == 1) return true;
+        
+        int l = start;
+        int r = end;
+            
         while(start <= end){
             if(s[start++] != s[end--]){
+                // dp[start][end] = -1;
                 return false; 
             }    
         }
-
+        
+        // dp[l][r] = 1;
         return true;
     }
     
-    void recurse(string s, vector<string> substrings, vector<vector<string>>& ans, int st){
+    void recurse(string s, vector<string> substrings, vector<vector<string>>& ans, int st, vector<vector<int>>& dp){
         if(st == s.size()){
             ans.push_back(substrings);
             return;
         } 
         
         for(int i = st; i < s.size(); i++){
-            if(isPalindrome(s, st, i)){
+            if(isPalindrome(s, st, i, dp)){
                 substrings.push_back(s.substr(st, i - st + 1));
-                recurse(s, substrings, ans, i+1);
+                recurse(s, substrings, ans, i+1, dp);
                 substrings.pop_back(); 
             } 
         }
@@ -33,7 +41,9 @@ public:
         vector<vector<string>> ans;
         vector<string> substrings;
         
-        recurse(s, substrings, ans, 0);
+        vector<vector<int>> dp(20, vector<int> (20, 0));
+        
+        recurse(s, substrings, ans, 0, dp);
         
         return ans;
     }
