5639bc8fc12ae7e0fb1b8e329c6f3a4e695b1147
Roatt Tilma <64038600+roatt-tilma@users.noreply.github.com>
2022-07-31 16:41:16 +0530

    Time: 415 ms (12.85%), Space: 140.8 MB (20.30%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    
+    bool isPalindrome(string s){
+        int l = 0;
+        int r = s.size() - 1;
+        
+        while(l < r){
+            if(s[l] != s[r]) return false;
+            l++;
+            r--;
+        }
+        
+        return true;
+    }
+    
+    void recurse(string s, vector<string> substrings, vector<vector<string>>& ans, int stringSize, int st){
+        if(st == stringSize){
+            ans.push_back(substrings);
+            return;
+        } 
+        
+        for(int i = 1; i <= stringSize - st; i++){
+            string potPalindrome = s.substr(st, i);
+            if(!isPalindrome(potPalindrome)) continue;
+            substrings.push_back(potPalindrome);
+            recurse(s, substrings, ans, stringSize, st+i);
+            substrings.pop_back();
+        }
+    }
+    
+    
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> substrings;
+        
+        recurse(s, substrings, ans, s.size(), 0);
+        
+        return ans;
+    }
+};
