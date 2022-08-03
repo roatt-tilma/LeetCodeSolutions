21902212d0ed40cac4bf92e05ba9a3db285657ce
Roatt Tilma <64038600+roatt-tilma@users.noreply.github.com>
2022-08-03 21:43:00 +0530

    Time: 16 ms (6.04%), Space: 8.8 MB (8.38%) - LeetHub

---
+++ 46-permutations/46-permutations.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    
+    void recurse(vector<int>& nums, vector<int> visited, vector<vector<int>>& ans, vector<int> permutation){
+        if(nums.size() == permutation.size()){
+            ans.push_back(permutation);
+            return; 
+        }
+        
+        for(int i = 0; i < nums.size(); i++){
+            if(!visited[i]){
+                visited[i] = 1;
+                permutation.push_back(nums[i]);
+                recurse(nums, visited, ans, permutation);
+                visited[i] = 0;
+                permutation.pop_back();
+            }
+        }
+        
+    }
+    
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> visited(nums.size(), 0);
+        vector<int> permutation;
+        
+        recurse(nums, visited, ans, permutation);
+        
+        return ans;
+    }
+};
