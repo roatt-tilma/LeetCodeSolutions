80fdad24f7a4b4e9218bbbc6c0c1dd2fbda30bb9
Roatt Tilma <64038600+roatt-tilma@users.noreply.github.com>
2022-08-07 10:55:24 +0530

    Time: 215 ms (7.74%), Space: 45.5 MB (10.79%) - LeetHub

---
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        make_heap(nums.begin(), nums.end());
+        k--;
+        while(k--){
+            pop_heap(nums.begin(), nums.end());
+            nums.pop_back();
+        }
+        
+        return nums.front();
+    }
+};
