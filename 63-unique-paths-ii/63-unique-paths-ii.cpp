e1097afcbecb10b7269d65ff5a2c0e0eaf498aae
Roatt Tilma <64038600+roatt-tilma@users.noreply.github.com>
2022-08-09 12:34:37 +0530

    Time: 3 ms (84.72%), Space: 7.8 MB (64.25%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,7 +1,10 @@
 class Solution {
 public:
     
-    int recurse(int i, int j, int m, int n, vector<vector<int>>& obstacleGrid, vector<vector<int>>& dp){
+    int recurse(int i, int j, vector<vector<int>>& obstacleGrid, vector<vector<int>>& dp){
+        
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
         
         if(i > m - 1 || j > n - 1 || obstacleGrid[i][j] == 1) return 0;
         
@@ -13,8 +16,8 @@ public:
         
         int count = 0;
         
-        count += recurse(i, j+1, m, n, obstacleGrid, dp);
-        count += recurse(i+1, j, m, n, obstacleGrid, dp);
+        count += recurse(i, j+1, obstacleGrid, dp);
+        count += recurse(i+1, j, obstacleGrid, dp);
           
         dp[i][j] = count;
         
@@ -22,12 +25,10 @@ public:
     }
     
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m = obstacleGrid.size();
-        int n = obstacleGrid[0].size();
         
-        vector<vector<int>> dp(m, vector<int>(n, -1));
+        vector<vector<int>> dp(obstacleGrid.size(), vector<int>(obstacleGrid[0].size(), -1));
         
-        return recurse(0,0,m,n,obstacleGrid,dp);
+        return recurse(0,0,obstacleGrid,dp);
         
     }
 };
