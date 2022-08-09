062e09b4aa2e22a68d013ba7177b60120a1dfe32
Roatt Tilma <64038600+roatt-tilma@users.noreply.github.com>
2022-08-09 21:51:13 +0530

    Time: 8 ms (7.14%), Space: 6.6 MB (26.61%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     
-    int countUPath(int m, int n, int i, int j, vector<vector<int>>& dp){
-        if(i == m || j == n) return 0;
-        if(i == m-1 && j == n-1) return 1;
+    int countUPath(int m, int n, vector<vector<int>>& dp){
+        if(m == 0 || n == 0) return 0;
+        if(m == 1 && n == 1) return 1;
         
-        if(dp[i][j] == -1) dp[i][j] = countUPath(m, n, i+1, j, dp) + countUPath(m, n, i, j+1, dp);
+        if(dp[m][n] == -1) dp[m][n] = countUPath(m-1, n, dp) + countUPath(m, n-1, dp);
     
-        return dp[i][j];
+        return dp[m][n];
     }
     
     
     int uniquePaths(int m, int n) {
-        //vector<vector<int>> dp(m, vector<int> (n, -1));
-        //return countUPath(m, n, 0, 0, dp);
+        vector<vector<int>> dp(m+1, vector<int> (n+1, -1));
+        return countUPath(m, n, dp);
         
-        int minimum = min(m-1,n-1);
+//         int minimum = min(m-1,n-1);
         
-        double ans = 1;
+//         double ans = 1;
         
-        for(int i = 1; i <= minimum; i++){
-            ans = ans * (m+n-2-minimum+i)/i;
-        }
+//         for(int i = 1; i <= minimum; i++){
+//             ans = ans * (m+n-2-minimum+i)/i;
+//         }
         
-        return (int)ans;
+//         return (int)ans;
         
     }
 };
