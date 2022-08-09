9f635a2f0688e508ffbab4897ea84a7f5ce00daa
Roatt Tilma <64038600+roatt-tilma@users.noreply.github.com>
2022-08-09 11:36:09 +0530

    Time: 4 ms (66.93%), Space: 7.9 MB (12.72%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -23,11 +23,28 @@ public:
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
         
-        if(obstacleGrid[0][0] == 1) return 0;
+//         if(obstacleGrid[0][0] == 1) return 0;
         
-        vector<vector<int>> dp(m, vector<int>(n, -1));
+//         vector<vector<int>> dp(m, vector<int>(n, -1));
         
-        return recurse(0,0,m,n,obstacleGrid,dp);
+//         return recurse(0,0,m,n,obstacleGrid,dp);
         
+        vector<vector<long long>> dp(m, vector<long long>(n, 0));
+        
+        for(int i = m-1; i >= 0; i--)
+            if(obstacleGrid[i][n-1] == 0) dp[i][n-1] = 1;
+            else break;
+        
+        for(int j = n-1; j >= 0; j--)
+            if(obstacleGrid[m-1][j] == 0) dp[m-1][j] = 1;
+            else break;
+        
+        for(int i = m-2; i >= 0; i--){
+            for(int j = n-2; j >= 0; j--){
+                if(obstacleGrid[i][j] == 0) dp[i][j] = dp[i+1][j] + dp[i][j+1];
+            }
+        }
+        
+        return dp[0][0];
     }
 };
