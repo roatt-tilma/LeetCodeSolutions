199537605c33ee650db3278f3a00b1563bb2685e
Roatt Tilma <64038600+roatt-tilma@users.noreply.github.com>
2022-08-12 21:31:58 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (25.28%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        //compare with x, search from 0 to x
+        
+        if(x == 0 || x == 1) return x;
+        
+        int l = 0;
+        int r = x/2;
+        
+        while(l <= r){
+            long long mid = (l+r)/2;
+            
+            long long sq = mid*mid;
+            
+            if(sq == x) return mid;
+            
+            if(sq > x) r = mid - 1;
+            if(sq < x) l = mid + 1;
+        }
+        
+        return r;
+        
+    }
+};
