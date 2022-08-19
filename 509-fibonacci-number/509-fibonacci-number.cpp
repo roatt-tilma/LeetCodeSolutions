0386893785a9a1dedba1c551e0a98d653e6a06ea
Roatt Tilma <64038600+roatt-tilma@users.noreply.github.com>
2022-08-19 22:49:36 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (39.80%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -10,18 +10,24 @@ public:
     
     
     int fib(int n) {
-        vector<int> dp(n+1);
+        // vector<int> dp(n+1);
         
         if(n < 2) return n;
         
-        dp[0] = 0;
-        dp[1] = 1;
+        // dp[0] = 0;
+        // dp[1] = 1;
+        
+        int pre1 = 0;
+        int pre2 = 1;
+        int ans = pre1 + pre2;
         
         for(int i = 2; i < n+1; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            ans = pre1 + pre2;
+            pre1 = pre2;
+            pre2 = ans;
         }
         
-        return dp[n];
+        return ans;
         
         // return recurse(n, dp);
     }
