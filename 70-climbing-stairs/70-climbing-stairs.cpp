0c4775b31db56b09c264b5a3d477f9e2d10bc647
Roatt Tilma <64038600+roatt-tilma@users.noreply.github.com>
2022-08-19 23:12:14 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (15.85%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -13,7 +13,20 @@ public:
     
     
     int climbStairs(int n) {
-        vector<int> dp(n+2, -1);
-        return recurse(0, n, dp);    
+        
+        if(n <= 2) return n;
+        
+        vector<int> dp(n+1);
+        
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for(int i = 3; i < n+1; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        
+        return dp[n];
+        
+        // return recurse(0, n, dp);    
     }
 };
