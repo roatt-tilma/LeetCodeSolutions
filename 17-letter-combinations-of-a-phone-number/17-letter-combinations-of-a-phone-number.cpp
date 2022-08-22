7613430625e025b29848d996fe0b04b6d4274166
Roatt Tilma <64038600+roatt-tilma@users.noreply.github.com>
2022-08-22 23:14:57 +0530

    Time: 5 ms (19.58%), Space: 6.8 MB (18.35%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    
+    void recurse(string digits, map<char,vector<char>>& mp, vector<string>& ans, string str, int index){
+        
+        if(digits.size() == 0) return;
+        
+        if(index >= digits.size()){
+            ans.push_back(str);
+            return;
+        }
+        
+        for(auto i: mp[digits[index]]){
+            str[index] = i;
+            recurse(digits, mp, ans, str, index+1);
+        }
+        
+    }
+    
+    vector<string> letterCombinations(string digits) {
+        vector<string> ans;
+        string str = digits;
+        map<char, vector<char>> mp {
+            {'2', {'a','b','c'}},
+            {'3', {'d','e','f'}},
+            {'4', {'g','h','i'}},
+            {'5', {'j','k','l'}},
+            {'6', {'m','n','o'}},
+            {'7', {'p','q','r','s'}},
+            {'8', {'t','u','v'}},
+            {'9', {'w','x','y','z'}}
+        };
+        
+        recurse(digits, mp, ans, str, 0);
+        
+        return ans;
+        
+    }
+};
