3c1c071451db179cd6ced80598ebf61db8ababcc
Roatt Tilma <64038600+roatt-tilma@users.noreply.github.com>
2022-08-25 18:40:18 +0530

    Time: 46 ms (18.57%), Space: 8.8 MB (39.49%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -2,18 +2,13 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
         map<char,int> magazineMap;
-        map<char,int> ransomNoteMap;
         
         for(auto letter: magazine){
             magazineMap[letter]++;
         }
         
         for(auto letter: ransomNote){
-            ransomNoteMap[letter]++;
-        }
-        
-        for(auto i: ransomNoteMap){
-            if(i.second > magazineMap[i.first]) return false; 
+            if(magazineMap[letter]-- <= 0) return false;
         }
         
         return true;
